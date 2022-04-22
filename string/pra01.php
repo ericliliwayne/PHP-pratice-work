fc62d4d0595c707fc30ee7728024bd31cc0ad4df
tung wei-i <ericliliwayne@gmail.com>
2022-04-22 16:42:13 +0800

    字串整合練習

--- string/pra01.php
+++ string/pra01.php
@@ -56,6 +56,51 @@
         $newstr=join(" ",$array);//或用implode(" ",$array)也可達成相同結果
         echo $newstr;
     ?>
+    <hr>
+    <h1>子字串取用</h1>
+    <article>
+        <h3>子字串取用</h3>
+        <div>
+        將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”
+        </div>
+    </article>
+    <?php
+        //$str="The reason why a great man is great is that he resolves to be a great man";
+        $str="有志者事竟成";
+        echo $str;
+        echo "<br>";
+        //$newstr=substr($str,0,10);//從0開始取10個字(字元)
+        $newstr=mb_substr($str,0,3);//有中文字用mb系列比較好，主流用法
+        echo $newstr."...";
+
+    ?>
+    <hr>
+    <h1>尋找字串與HTML、css整合應用</h1>
+    <article>
+        <h3>尋找字串與HTML、css整合應用</h3>
+        <div>
+        給定一個句子，將指定的關鍵字放大
+        “學會PHP網頁程式設計，薪水會加倍，工作會好找”
+        請將上句中的 “程式設計” 放大字型或變色.
+        </div>
+    </article>
+    <?php
+        $str="學會PHP網頁程式設計，薪水會加倍，工作會好找";
+        echo $str;
+        echo "<br>";
+        $search="程式設計";
+        $pos=mb_strpos($str,$search);
+        $hand=mb_substr($str,0,$pos);
+        $tail=mb_substr($str,$pos+mb_strlen($search));
+        $str=$hand
+        ."<span style='font-size:2rem;color:red'>"
+        .$search
+        ."</span>"
+        ."，薪水會加倍，工作會好找";
+        echo $str;
+        echo "<br>";
+        //echo "學會PHP網頁<span style='font-size:2rem;color:red'>程式設計</span>，薪水會加倍，工作會好找";
+    ?>
 
 </body>
 </html>
